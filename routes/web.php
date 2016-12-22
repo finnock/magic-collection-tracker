4e49412190a3fa96b6b46fcdd449c566b226205a
Finnock <jan.oechsler+github@gmail.com>
2016-12-22 12:05:55 +0100

    created a new layout page for bootstrap created a new home view deleted boilerplate welcome view add auth stuff new card resource -rmc

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/', function (){
+    return view('home');
+});
+
+Route::resource('Card', 'CardController');
